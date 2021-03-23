3f9be30a0da1755b03b3286e2d070fff25e6a16d
labadcloyd <abadcloyd@gmail.com>
2021-03-23 19:40:27 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const next = require('next')
+
+const port = process.env.PORT || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+require('dotenv').config({path:'./config.env'})
+const connectDB=require('./utilsServer/connectDB')
+connectDB();
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.get('/a', (req, res) => {
+    return app.render(req, res, '/a', req.query)
+  })
+
+  server.get('/b', (req, res) => {
+    return app.render(req, res, '/b', req.query)
+  })
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
